e60f6d1d513a0fb4dcf4a5b409c35f80c53cc06c
Jeuneau <boekhoudtjeuneau@gmail.com>
2024-11-06 14:27:37 +0100

    Start

---
+++ src/Player.cs
@@ -0,0 +1,34 @@
+class Player
+{
+    // auto property
+    public Room CurrentRoom { get; set; }
+    public Room currentRoom;
+    public int health;
+    private int amount;
+
+    // constructor
+    public Player()
+    {
+        CurrentRoom = null;
+        health = 100;
+        amount = 10;
+    }
+
+    public void Damage(int amount)
+    {
+        health -= amount;
+    }
+   
+   public void Heal(int amount)
+   {
+       health += amount;
+   }
+
+   public void IsAlive()
+   {
+       if (health <= 0)
+       {
+           Console.WriteLine("You have died.");
+       }
+   }
+}
